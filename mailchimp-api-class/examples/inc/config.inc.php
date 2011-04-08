60c6d664f73a22cce9fbb74ea52ebf0ba7a449b4
Farhang Darzi <farhang.darzi@gmail.com>
2011-04-08 17:49:23 +0430

    now code can receive the list from mailchimp server, but now only for testing enviroment.

--- mailchimp-api-class/examples/inc/config.inc.php
+++ mailchimp-api-class/examples/inc/config.inc.php
@@ -1,11 +1,11 @@
 <?php
     //API Key - see http://admin.mailchimp.com/account/api
-    $apikey = 'YOUR MAILCHIMP APIKEY';
-    
+    $apikey = 'f26fed3956736322a5bfb4db96558441-us2';
+
     // A List Id to run examples against. use lists() to view all
     // Also, login to MC account, go to List, then List Tools, and look for the List ID entry
     $listId = 'YOUR MAILCHIMP LIST ID - see lists() method';
-    
+
     // A Campaign Id to run examples against. use campaigns() to view all
     $campaignId = 'YOUR MAILCHIMP CAMPAIGN ID - see campaigns() method';
 
@@ -15,5 +15,6 @@
 
     //just used in xml-rpc examples
     $apiUrl = 'http://api.mailchimp.com/1.3/';
-    
+
 ?>
+
